052e0e8114dcbd2e908eecf510bd7171101c4c96
김진환 <stux12@naver.com>
2026-07-13 21:34:21 +0900

    feat: add JH mark to navigation

--- app/components/top-link.tsx
+++ app/components/top-link.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { MouseEvent } from "react";
+import Image from "next/image";
 
 export default function TopLink() {
   const moveToTop = (event: MouseEvent<HTMLAnchorElement>) => {
@@ -10,7 +11,8 @@ export default function TopLink() {
 
   return (
     <a className="wordmark" href="#top" aria-label="페이지 최상단으로 이동" onClick={moveToTop}>
-      진환&apos;s 포트폴리오
+      <Image src="/og-jh.png" alt="" width={30} height={30} />
+      <span>진환&apos;s 포트폴리오</span>
     </a>
   );
 }
