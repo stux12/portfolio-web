69b903624219f4cdaf4f54c4a5b048c1f8b94329
김진환 <stux12@naver.com>
2026-07-13 21:45:35 +0900

    fix: simplify social image and restore navigation

--- app/components/top-link.tsx
+++ app/components/top-link.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { MouseEvent } from "react";
-import Image from "next/image";
 
 export default function TopLink() {
   const moveToTop = (event: MouseEvent<HTMLAnchorElement>) => {
@@ -11,8 +10,7 @@ export default function TopLink() {
 
   return (
     <a className="wordmark" href="#top" aria-label="페이지 최상단으로 이동" onClick={moveToTop}>
-      <Image src="/og-jh.png" alt="" width={30} height={30} />
-      <span>진환&apos;s 포트폴리오</span>
+      진환&apos;s 포트폴리오
     </a>
   );
 }
